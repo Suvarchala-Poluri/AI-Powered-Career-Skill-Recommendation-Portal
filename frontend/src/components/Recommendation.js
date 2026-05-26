ec0c9a1306b8dae51c202ee1baef94211d09906d
Suvarchala Devi Poluri <polurisuvarchala2003@gmail.com>
2026-05-26 15:21:36 +0530

    Create Recommendation.js

---
+++ frontend/src/components/Recommendation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Recommendation() {
+
+    return (
+        <div>
+
+            <h2>Recommendation Page</h2>
+
+        </div>
+    );
+}
+
+export default Recommendation;
