634be2c04831e6d2260feeb61288a4f7738fd580
Suvarchala Devi Poluri <polurisuvarchala2003@gmail.com>
2026-05-26 15:18:53 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ResumeUpload from './components/ResumeUpload';
+
+function App() {
+  return (
+    <div>
+      <h1>AI Career Recommendation Portal</h1>
+      <ResumeUpload />
+    </div>
+  );
+}
+
+export default App;
